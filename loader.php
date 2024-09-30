5fb060f8768a3dc43e74b2986b3dbf91ceb3f6be
coderjahidul <sobuz0349@gmail.com>
2024-09-30 16:13:21 +0600

    add plugin all Boilerplate

---
+++ loader.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * Load files for plugin which are not responsible for autoloading.
+ *
+ * @link              https://github.com/mtmsujan
+ * @since             1.0.0
+ * @package           shipgate-api-integration
+ */
+
+// include helper functions
+include_once PLUGIN_BASE_PATH . '/inc/helpers/helper-functions.php';
+
+// include file fetch shipping method list file
+include_once PLUGIN_BASE_PATH . '/inc/fetch-shipping-method-list.php';
